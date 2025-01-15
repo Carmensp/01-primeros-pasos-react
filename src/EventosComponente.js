48cc03a927e5d09e9d6f9db11b1333bc2e5cdd80
carmensp <carmen.8.2.0.3.sp@gmail.com>
2025-01-15 12:03:50 +0100

    segundo commit

--- src/EventosComponente.js
+++ src/EventosComponente.js
@@ -5,11 +5,37 @@ export const EventosComponente = () => {
     const hasDadoClick = (e, nombre) => {
         alert("Has dado click al botón!! " + nombre)
     }
+    const hasDadoDobleClick = (e, nombre) => {
+        alert("Has dado doble click al botón!! " + nombre)
+    }
+    const hasEntrado = (e) => {
+        console.log("Has dentrado a la caja!!")
+    }
+    const hasSalido = (e) => {
+        console.log("Has salido de la caja!!")
+    }
+    const estasDentro = (e) => {
+        console.log("Estás dentro del input. Mete tu nombre")
+    }
+    const estasFuera = (e) => {
+        console.log("Estás fuera del input. Adiós")
+    }
 
   return (
     <div>
         <h1>Eventos en React</h1>
-        <button onClick={e => hasDadoClick(e, "Carmen")}>Dame click!</button>
+        <p>
+          <button onClick={e => hasDadoClick(e, "Carmen")}>Dame click!</button>
+        </p>
+        <p>
+          <button onDoubleClick={e => hasDadoDobleClick(e, "Carmen")}>Dame doble click!</button>
+        </p>
+        <div id='caja' onMouseEnter={hasEntrado} onMouseLeave={hasSalido}>
+          Pasa por encima
+        </div>
+        <p>
+          <input type="text" onFocus={estasDentro} onBlur={estasFuera} placeholder='Introduce tu nombre'/>
+        </p>
     </div>
   )
 }
